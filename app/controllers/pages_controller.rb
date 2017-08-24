38c1be3ff8d64a5636e619ff65f5166e3218ef86
Jonathan Norstrom <jon.norstrom@gmail.com>
2017-08-24 19:45:36 -0400

    get dropdown to appear with addresses

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
 
   def index
+    @url = get_url
+  end
 
+  private
+
+  def get_url
+    "https://maps.googleapis.com/maps/api/js?key=#{ENV['GOOGLE_MAP_API']}&libraries=places&callback=initAutocomplete"
   end
 end
